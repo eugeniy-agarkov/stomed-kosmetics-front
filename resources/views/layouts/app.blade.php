1cfe62a01e5fa29e9909c1e19f6054d00d409e4c
Alex <remstroyod@gmail.com>
2022-08-09 11:24:31 +0300

    Add Yandex

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
+    <meta name="yandex-verification" content="5e122e44c341b30e" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
